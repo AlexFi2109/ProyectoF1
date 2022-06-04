1f83235b48d1b9db724709e37c3ac0298875bce4
AlexFi2109 <alexsanled1505@gmail.com>
2022-06-04 18:34:50 -0500

    EnemigoA
    El enemigo y todo lo que conlleva

---
+++ Assets/Monster/EnemyScript.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public int caso;
+    public float contador;
+    public Animator animator;
+    public float danoPunch, danoFlip, danoPunch2, danoSlash, danoJump, danoRoar, vida;
+
+    public GameObject objetivo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        objetivo = GameObject.Find("Xaya");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (vida <= 0)
+        {
+            animator.SetBool("AttackPunch", false);
+            animator.SetBool("AttackFlip", false);
+            animator.SetBool("AttackPunch2", false);
+            animator.SetBool("AttackSlash", false);
+            animator.SetBool("AttackJump", false);
+            animator.SetBool("Walk", false);
+            animator.SetBool("Reaction", false);
+            animator.SetBool("Death", true);
+            contador = 0;
+            contador += Time.deltaTime;
+            if (contador >= 3)
+            {
+                Destroy(gameObject);
+            }
+
+        }
+
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        Ataque();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        animator.SetBool("AttackPunch", false);
+        animator.SetBool("AttackFlip", false);
+        animator.SetBool("AttackPunch2", false);
+        animator.SetBool("AttackSlash", false);
+        animator.SetBool("AttackJump", false);
+        animator.SetBool("Walk", true);
+    }
+
+    private void Ataque()
+    {
+        contador += Time.deltaTime;
+        if (contador >= 1.5)
+        {
+            caso = Random.Range(1, 6);
+            contador = 0;
+        }
+        switch (caso)
+        {
+            case 0:
+                animator.SetBool("AttackPunch", false);
+                animator.SetBool("AttackFlip", false);
+                animator.SetBool("AttackPunch2", false);
+                animator.SetBool("AttackSlash", false);
+                animator.SetBool("AttackJump", false);
+                animator.SetBool("Walk", true);
+                break;
+
+            case 1:
+                animator.SetBool("AttackPunch", false);
+                animator.SetBool("AttackFlip", true);
+                animator.SetBool("AttackPunch2", false);
+                animator.SetBool("AttackSlash", false);
+                animator.SetBool("AttackJump", false);
+                animator.SetBool("Walk", false);
+                break;
+
+            case 2:
+                animator.SetBool("AttackPunch", true);
+                animator.SetBool("AttackFlip", false);
+                animator.SetBool("AttackPunch2", false);
+                animator.SetBool("AttackSlash", false);
+                animator.SetBool("AttackJump", false);
+                animator.SetBool("Walk", false);
+                break;
+            case 3:
+                animator.SetBool("AttackPunch", false);
+                animator.SetBool("AttackFlip", false);
+                animator.SetBool("AttackPunch2", true);
+                animator.SetBool("AttackSlash", false);
+                animator.SetBool("AttackJump", false);
+                animator.SetBool("Walk", false);
+                break;
+            case 4:
+                animator.SetBool("AttackPunch", false);
+                animator.SetBool("AttackFlip", false);
+                animator.SetBool("AttackPunch2", false);
+                animator.SetBool("AttackSlash", true);
+                animator.SetBool("AttackJump", false);
+                animator.SetBool("Walk", false);
+                break;
+            case 5:
+                animator.SetBool("AttackPunch", false);
+                animator.SetBool("AttackFlip", false);
+                animator.SetBool("AttackPunch2", false);
+                animator.SetBool("AttackSlash", false);
+                animator.SetBool("AttackJump", true);
+                animator.SetBool("Walk", false);
+                break;
+        }
+    }
+
+    public void RecibeDaño(float danoRecibido)
+    {
+        vida = vida - danoRecibido;
+        
+        if(danoRecibido >= 50) //El valor puede aumentar dependiendo 
+        {
+            
+            animator.SetBool("AttackPunch", false);
+            animator.SetBool("AttackFlip", false);
+            animator.SetBool("AttackPunch2", false);
+            animator.SetBool("AttackSlash", false);
+            animator.SetBool("AttackJump", false);
+            animator.SetBool("Walk", false);
+            animator.SetBool("Reaction", true);
+        }
+        
+
+    }
+
+}
+
---
+++ Assets/Monster/MonsterScript.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterScript : MonoBehaviour
+{
+    public float vida, contador,contadorA,a;
+    public float danoPunch,danoFlip,danoPunch2,danoSlash,danoJump,danoRoar,danoRecibido;
+    public Animator animator;
+    public int seleccionador;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        contador += Time.deltaTime;
+        if(contador > 4)
+        {
+            contador = 0;
+            a = 1;
+            Ataque();
+        }     
+    }
+
+    public void Ataque()
+    {
+        seleccionador = Random.Range(0, 2);
+        contador += Time.deltaTime;
+        if (seleccionador == 0)
+        {
+            animator.SetBool("AttackFlip", true);
+            animator.SetBool("Walk", false);
+            Esperar(2);
+            seleccionador = Random.Range(0, 2);
+            contador += Time.deltaTime;
+            if (seleccionador == 0 && contador > 5)
+            {
+                contador = 0;
+                animator.SetBool("AttackPunch2", true);
+
+            }
+            animator.SetBool("AttackPunch2", false);
+        }
+        animator.SetBool("AttackFlip", false);
+        
+
+        if (seleccionador == 1)
+        {
+            animator.SetBool("AttackPunch", true);
+            animator.SetBool("Walk", false);
+            Esperar(2);
+            seleccionador = Random.Range(0, 3);
+            contadorA = 0;
+            contadorA += Time.deltaTime;
+            if (seleccionador == 0 && contadorA > 2)
+            {
+
+                animator.SetBool("AttackSlash", true);
+                
+
+            }
+            else if (seleccionador == 1 && contadorA > 2)
+            {
+                animator.SetBool("AttackJump", true);
+                
+
+            }
+            else if (contadorA > 5)
+            {
+                animator.SetBool("AttackFlip", true);
+                animator.SetBool("AttackPunch", false);
+                
+            }
+        }
+        contador = 0;
+    }
+
+    private IEnumerator Esperar(int s)
+    {
+        yield return new WaitForSeconds(s);
+    }
+
+
+}
