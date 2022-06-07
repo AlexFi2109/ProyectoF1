61a773dfb6ad042904b8c7d24c72b110f9193f30
AlexFi2109 <alexsanled1505@gmail.com>
2022-06-07 16:28:39 -0500

    Arco v2

--- Assets/Arco/ArcoScript.cs
+++ Assets/Arco/ArcoScript.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class ArcoScript : MonoBehaviour
 {
     public Animator animator;
-    private float counter;
+    private float counter,counter2;
     public float timeToShoot;
     public float energy;
     public float shootForce;
     public float energyNeededToShoot;
     public GameObject shootPoint;
     public GameObject prefabToShoot;
+    public GameObject arco;
     private GameObject shootedPrefab;
+    private int a=0; //Condicion para que no genere dos flechas encimadas
+    private float h;
 
     // Start is called before the first frame update
     void Start()
@@ -24,20 +27,32 @@ public class ArcoScript : MonoBehaviour
     void Update()
     {
         counter += Time.deltaTime;
-
-        if (counter >= timeToShoot && Input.GetButtonDown("Fire1") && energy >= energyNeededToShoot)
+        counter2 += Time.deltaTime;
+        
+        if (counter >= timeToShoot && Input.GetButtonDown("Fire1") && energy >= energyNeededToShoot && a==0)
         {
             shootedPrefab = Instantiate(prefabToShoot, shootPoint.transform.position, shootPoint.transform.rotation);
+            a = 1;
             animator.SetBool("Apunta", true);
             animator.SetBool("Dispara", false);
+            shootedPrefab.transform.parent = arco.transform;
         }
         if (counter >= timeToShoot + 0.15 && Input.GetButtonDown("Fire2") && energy >= energyNeededToShoot)
         {
+            shootedPrefab.GetComponent<Rigidbody>().useGravity = true;
             counter = 0;
             energy -= energyNeededToShoot;
             animator.SetBool("Apunta", false);
             animator.SetBool("Dispara", true);
+            counter2 = 0;
             shootedPrefab.GetComponent<Rigidbody>().AddRelativeForce(shootForce * Vector3.up);
+            a = 0;
+            h = shootedPrefab.GetComponent<Transform>().position.y;
+        }
+        if (h <= 0.1 && a == 0)
+        {
+            Destroy(shootedPrefab);
+            counter2 = 0;
         }
 
     }
---
+++ Assets/Arco/FlechaScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlechaScript : MonoBehaviour
+{
+    public float h;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        h = GetComponent<Transform>().position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (h<=2)
+        {
+            Destroy(this);
+        }
+    }
+
+}
