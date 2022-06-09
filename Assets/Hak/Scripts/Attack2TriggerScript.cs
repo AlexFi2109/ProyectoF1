bd1c0159f171cbdb4dbde8737ab440de1981447d
Brenda <brendaa.aguilarp@gmail.com>
2022-06-09 01:34:15 -0500

    Hak

---
+++ Assets/Hak/Scripts/Attack2TriggerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack2TriggerScript : MonoBehaviour
+{
+    private void OnTriggerStay(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Player"))
+        {
+            GetComponentInParent<HakScript>().magicAttack = false;
+            GetComponentInParent<HakScript>().isMagicAttacking = false;
+            GetComponentInParent<HakScript>().attack2 = true;
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            GetComponentInParent<HakScript>().attack2 = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            GetComponentInParent<HakScript>().attack2 = false;
+        }
+    }
+}
