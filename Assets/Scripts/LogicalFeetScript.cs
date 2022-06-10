9562da9a402012a87e42b64655885c3b09b1e125
TMonttiT <dana_hernan@comunidad.unam.mx>
2022-06-10 06:35:05 -0500

    Xaya

---
+++ Assets/Scripts/LogicalFeetScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicalFeetScript : MonoBehaviour
+{
+    public FPSControllerXaya logicFeetXaya;
+
+    private void OnTriggerStay(Collider other)
+    {
+        logicFeetXaya.jump = true;
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        logicFeetXaya.jump = false;
+    }
+}
---
+++ Assets/Scripts/LogicalHeadScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LogicalHeadScript : MonoBehaviour
+{
+    public int timerCollider = 0;       
+
+    void OnTriggerEnter(Collider other)
+    {
+        timerCollider++;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        timerCollider--;
+    }
+}
