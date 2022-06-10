4db50bda6413ef3fb18a40198bef2c8a3a8ecfa4
Edufigreat <edufiguegro@gmail.com>
2022-06-10 10:48:23 -0500

    MuroInvisibleGuerreros

---
+++ Assets/Warrior/Extras/InvisibleWallScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InvisibleWallScript : MonoBehaviour
+{
+    public GameObject enemie1;
+    public GameObject enemie2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemie1.GetComponent<EnemyHealthScript>().enemyHealth <= 0 &&
+            enemie2.GetComponent<EnemyHealthScript>().enemyHealth <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
